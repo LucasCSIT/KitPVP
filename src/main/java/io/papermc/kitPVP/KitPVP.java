e1e218f04edf5d57bf164c565c484733d5b552db
Lucas Garcia <lucasisawesome@icloud.com>
2026-02-13 23:43:13 -0500

    apply stats for Tank kit

--- src/main/java/io/papermc/kitPVP/KitPVP.java
+++ src/main/java/io/papermc/kitPVP/KitPVP.java
@@ -2,8 +2,15 @@ package io.papermc.kitPVP;
 
 import io.papermc.kitPVP.commands.ToggleKitPVPCommand;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.*;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class KitPVP extends JavaPlugin implements Listener {
   public static boolean isPluginEnabled = false;
@@ -12,6 +19,48 @@ public class KitPVP extends JavaPlugin implements Listener {
   public void onEnable() {
     Bukkit.getPluginManager().registerEvents(this, this);
     registerCommands();
+  }
+
+  @EventHandler
+  public void onPlayerIsTank(PlayerInteractEvent event) {
+    if (isPluginEnabled) {
+      return;
+    }
+
+    Player player = event.getPlayer();
+    setTankStats(player);
+  }
+
+  private void setTankStats(Player player) {
+    ItemStack helmet = player.getInventory().getHelmet();
+    ItemStack chestplate = player.getInventory().getChestplate();
+    ItemStack leggings = player.getInventory().getLeggings();
+    ItemStack boots = player.getInventory().getLeggings();
+    int diamondArmorCount = 0;
+
+    if (null == helmet || null == chestplate || null == leggings || null == boots) {
+      player.sendMessage("Not full diamond. Returning");
+      return;
+    }
+
+    if (helmet.getType() == Material.DIAMOND_HELMET) {
+      diamondArmorCount++;
+    }
+    if (chestplate.getType() == Material.DIAMOND_CHESTPLATE) {
+      diamondArmorCount++;
+    }
+    if (leggings.getType() == Material.DIAMOND_LEGGINGS) {
+      diamondArmorCount++;
+    }
+    if (boots.getType() == Material.DIAMOND_BOOTS) {
+      diamondArmorCount++;
+    }
+
+    if (diamondArmorCount == 4) {
+      player.sendMessage("Diamond armor stuff set");
+      player.addPotionEffect(new PotionEffect(PotionEffectType.SLOWNESS, 999999, 1));
+      player.setSprinting(false);
+    }
   }
 
   private void registerCommands() {

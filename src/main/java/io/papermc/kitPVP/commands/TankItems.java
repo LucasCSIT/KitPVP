e8f480944a288287a13bd4a37106ecd4341d80ac
Lucas Garcia <lucasisawesome@icloud.com>
2026-02-23 22:31:51 -0500

    init TankItems class

---
+++ src/main/java/io/papermc/kitPVP/commands/TankItems.java
@@ -0,0 +1,13 @@
+package io.papermc.kitPVP.commands;
+
+import io.papermc.kitPVP.KitPVP;
+import io.papermc.paper.command.brigadier.BasicCommand;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+import net.kyori.adventure.text.Component;
+
+public class TankItems implements BasicCommand {
+  @Override
+  public void execute(CommandSourceStack source, String[] args) {
+
+  }
+}

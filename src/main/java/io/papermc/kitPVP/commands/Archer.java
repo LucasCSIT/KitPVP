493797f98dcc639d28cacb45a455e724c6ead5d5
Lucas Garcia <lucasisawesome@icloud.com>
2026-02-25 21:49:28 -0500

    feat: implement `Archer.java`

--- src/main/java/io/papermc/kitPVP/commands/Archer.java
+++ src/main/java/io/papermc/kitPVP/commands/Archer.java
@@ -1,10 +1,19 @@
 package io.papermc.kitPVP.commands;
 
+import io.papermc.kitPVP.KitPVP;
+import io.papermc.kitPVP.common.KitPVPArmor;
+import io.papermc.kitPVP.common.KitSetup;
 import io.papermc.paper.command.brigadier.BasicCommand;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffectType;
 
-public class Archer implements BasicCommand {
+public class Archer extends KitPVPArmor implements BasicCommand, KitSetup {
   boolean isArcherEquipped = false;
 
   @Override
@@ -12,5 +21,62 @@ public class Archer implements BasicCommand {
     final Component name = null != source.getExecutor()
         ? source.getExecutor().name()
         : source.getSender().name();
+
+    if (isCommandArgsEmpty(args)) {
+      source.getSender().sendRichMessage("Missing arguments. /archer <equip|unequip>");
+    }
+
+    final String message = String.join(" ", args);
+    Player player = (Player) source.getSender();
+    Component toggleMessage = null;
+    PotionEffectType[] effectTypes = new PotionEffectType[1];
+    Material[] armorPieces = new Material[4];
+
+    if (!KitPVP.isPluginEnabled) {
+      toggleMessage = MiniMessage.miniMessage().deserialize(
+          "[<red><bold>ALERT</red>] The KitPVP plugin is disabled! Enable it by typing /kitpvp enable.",
+          Placeholder.component("name", name)
+      );
+      return;
+    }
+    if (message.equalsIgnoreCase("equip")) {
+      if (isArcherEquipped) {
+        player.sendMessage("The Archer class is already equipped!");
+        return;
+      }
+      clearInventory(player);
+      effectTypes[0] = PotionEffectType.JUMP_BOOST;
+      setStats(player, effectTypes, 1, true);
+      giveWeaponry(player, Material.WOODEN_SWORD);
+      giveWeaponry(player, Material.BOW);
+      armorPieces[0] = Material.CHAINMAIL_HELMET;
+      armorPieces[1] = Material.CHAINMAIL_CHESTPLATE;
+      armorPieces[2] = Material.CHAINMAIL_LEGGINGS;
+      armorPieces[3] = Material.CHAINMAIL_BOOTS;
+      giveArmor(player, armorPieces);
+      toggleMessage = MiniMessage.miniMessage().deserialize(
+          "[<red><bold>ALERT</red>] <dark_gray><name></dark_gray> has unequipped the <white><bold>Archer</white> class.",
+          Placeholder.component("name", name)
+      );
+      isArcherEquipped = true;
+    } else if (message.equalsIgnoreCase("unequip")) {
+      if (!isArcherEquipped) {
+        player.sendMessage("The Archer is already unequipped!");
+        return;
+      }
+      removeStats(player);
+      clearInventory(player);
+      toggleMessage = MiniMessage.miniMessage().deserialize(
+          "[<red><bold>ALERT</red>] <dark_gray><name></dark_gray> has unequipped the <white><boldArcher</white> class.",
+          Placeholder.component("name", name)
+      );
+      isArcherEquipped = false;
+    } else {
+      player.sendMessage("Invalid arguments: /archer <equip|unequip>");
+    }
+
+    if (null != toggleMessage) {
+      Bukkit.broadcast(toggleMessage);
+    }
   }
 }

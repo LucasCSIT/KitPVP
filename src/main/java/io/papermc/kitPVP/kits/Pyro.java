5765a7641c08c1d9891961a2c0b02df7524582e5
Lucas Garcia <lucasisawesome@icloud.com>
2026-03-01 21:50:43 -0500

    feat: init Pyro class

---
+++ src/main/java/io/papermc/kitPVP/kits/Pyro.java
@@ -0,0 +1,13 @@
+package io.papermc.kitPVP.kits;
+
+import io.papermc.kitPVP.common.Kit;
+import io.papermc.paper.command.brigadier.BasicCommand;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+public class Pyro extends Kit implements BasicCommand {
+  @Override
+  public void execute(CommandSourceStack source, String @NonNull [] args) {
+
+  }
+}

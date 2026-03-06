8e6e7cf0fc20302d0633422b159cdb726e34a966
Lucas Garcia <lucasisawesome@icloud.com>
2026-03-05 23:16:57 -0500

    feat: implement Pyro command

--- src/main/java/io/papermc/kitPVP/kits/Pyro.java
+++ src/main/java/io/papermc/kitPVP/kits/Pyro.java
@@ -1,13 +1,74 @@
 package io.papermc.kitPVP.kits;
 
+import io.papermc.kitPVP.KitPVP;
 import io.papermc.kitPVP.common.Kit;
 import io.papermc.paper.command.brigadier.BasicCommand;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
-import org.checkerframework.checker.nullness.qual.NonNull;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffectType;
+import org.jspecify.annotations.NonNull;
 
 public class Pyro extends Kit implements BasicCommand {
+  Material[] armor = new Material[4];
+  Material[] weapons = new Material[4];
+  PotionEffectType[] potionEffectTypes = new PotionEffectType[0];
+
   @Override
   public void execute(CommandSourceStack source, String @NonNull [] args) {
+    final Component name = null != source.getExecutor()
+        ? source.getExecutor().name()
+        : source.getSender().name();
 
+    if (isCommandArgsEmpty(args)) {
+      source.getSender().sendRichMessage("Missing arguments. /pyro <equip|unequip>");
+    }
+
+    final String message = String.join(" ", args);
+    Player player = (Player) source.getSender();
+    Component toggleMessage = null;
+
+    if (!KitPVP.isPluginEnabled) {
+      toggleMessage = MiniMessage.miniMessage().deserialize(
+          "[<red><bold>ALERT</red>] The KitPVP plugin is disabled! Enable it by typing /kitpvp enable.",
+          Placeholder.component("name", name)
+      );
+      announce(toggleMessage);
+      return;
+    }
+    if (message.equalsIgnoreCase("equip")) {
+      setArmor();
+      setWeapons();
+      equipKit(player, armor, weapons, potionEffectTypes, true);
+      toggleMessage = MiniMessage.miniMessage().deserialize(
+          "[<red><bold>ALERT</red>] <dark_gray><name></dark_gray> has equipped the <orange><bold>Pyro</orange> class.",
+          Placeholder.component("name", name)
+      );
+    } else if (message.equalsIgnoreCase("unequip")) {
+      removeStats(player);
+      clearInventory(player);
+      toggleMessage = MiniMessage.miniMessage().deserialize(
+          "[<red><bold>ALERT</red>] <dark_gray><name></dark_gray> has unequipped the <orange><bold>Archer</orange> class.",
+          Placeholder.component("name", name)
+      );
+    } else {
+      player.sendMessage("Invalid arguments: /pyro <equip|unequip>");
+    }
+    announce(toggleMessage);
+  }
+
+  private void setArmor() {
+    armor[0] = Material.LEATHER_HELMET;
+    armor[1] = Material.LEATHER_CHESTPLATE;
+    armor[2] = Material.LEATHER_LEGGINGS;
+    armor[3] = Material.LEATHER_BOOTS;
+  }
+
+  private void setWeapons() {
+    weapons[1] = Material.WOODEN_SWORD;
+    weapons[2] = Material.FLINT_AND_STEEL;
   }
 }
